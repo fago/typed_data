9827511ea0489245bcae2431d8c99595406fc6e7
fago <nuppla@zites.net>
2016-12-02 18:31:02 +0100

    Add a basic text input widget.

--- src/Widget/FormWidgetBase.php
+++ src/Widget/FormWidgetBase.php
@@ -2,11 +2,50 @@
 
 namespace Drupal\typed_data\Widget;
 
+use Drupal\Component\Utility\NestedArray;
 use Drupal\Core\Plugin\PluginBase;
 
 /**
  * Base class for 'form widget' plugin implementations.
  */
 abstract class FormWidgetBase extends PluginBase implements FormWidgetInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition) {
+    parent::__construct($configuration + $this->defaultConfiguration(), $plugin_id, $plugin_definition);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConfiguration() {
+    return $this->configuration;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setConfiguration(array $configuration) {
+    $this->configuration = NestedArray::mergeDeep(
+      $this->defaultConfiguration(),
+      $configuration
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function calculateDependencies() {
+    return [];
+  }
 
 }
